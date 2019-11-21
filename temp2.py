0cc8e2b970468139455c985cb0295ad589f97f09
Akash <akashgurram18@gmail.com>
2019-11-21 17:01:40 -0500

    1st two rows complete along with Struct Hazard

--- temp2.py
+++ temp2.py
@@ -21,10 +21,10 @@ class Instructions:
         self.raw = 0
         self.war = 0
         self.waw = 0
-        self.sruct = 0
+        self.struct = "N"
         self.status = 0
-        self.int = 0
-        self.mem = 0
+        self.intCount = 1
+        self.memCount = 2
         
         if(len(inst) == 3):
             self.op1 = inst[1]
@@ -51,7 +51,7 @@ class Pipeline:
          #Iterating Over the instruction objects
          processor = Processor()
          print(processor.dBusy)
-         x = 2
+         x = 10
          cycle = 0
          while(x>0):
              cycle +=1
@@ -73,13 +73,48 @@ class Pipeline:
                              inst.decode = cycle
                              inst.status = 2
                              
+                     elif(inst.status == 2):
+                         print(inst.iname)
+                         if(inst.iname == "L.D"):
+                             
+                                                              
+                             if(processor.intBusy == "No" and inst.intCount == 1):
+                                 print(cycle,instObjs.index(inst))
+                                 processor.intBusy = "Yes" 
+                                 inst.intCount -=1
+                                 processor.dBusy = "No"
+                             
+                             elif(processor.memBusy == "No"):
+                                 if(inst.memCount == 2):
+                                     inst.memCount = 1
+                                     processor.intBusy = "No"
+                                     processor.memBusy = "Yes"
+                             elif(processor.memBusy == "Yes"):
+                                 if(inst.memCount == 2):
+                                     inst.struct = "Y"
+                                 if(inst.memCount == 1):                                
+                                     inst.memCount = 0
+                                     inst.exec = cycle
+
+                                     inst.status = 3
+#                                     processor.memCount = 2
+                                     
+                     elif(inst.status == 3):
+                         processor.memBusy = "No"
+                         inst.write = cycle
+                         inst.status = 4
+                            
+                                                                                                                            
+                             
+                             
                      
                      
                      
                  
-             x-=1
+             x -= 1
+         
          for i in instObjs:
-             print(i.fetch,i.decode,i.exec, i.write)
+             print(i.fetch,i.decode,i.exec, i.write, i.struct)
          print(functional_units.values())
          
 
@@ -87,8 +122,10 @@ class Processor:
     def __init__(self):
         self.fBusy = "No"
         self.dBusy = "No"
-        self.exBusy = "No"
         self.wbBusy = "No"
+        self.intBusy = "No"
+        self.memBusy = "No"
+        self.memCount = 2
                 
          
 
