84ed639990550cbe3ae10c0ab112730f126e15c1
Akash <akashgurram18@gmail.com>
2019-11-22 09:55:00 -0500

    Migrated to PyCharm and MULT.D working

--- temp2.py
+++ temp2.py
@@ -27,148 +27,158 @@ class Instructions:
         self.memCount = 2
         self.functionalUnit = None
         self.execCycle = 0
-        
-        if(len(inst) == 3):
+
+        if len(inst) == 3:
             self.op1 = inst[1]
             self.op2 = inst[2]
-            
-        if(len(inst) == 4):
+
+        if len(inst) == 4:
             self.op1 = inst[1]
             self.op2 = inst[2]
             self.op3 = inst[3]
-            
-        if(self.iname == "ADD.D" or self.iname == "SUB.D"):
+
+        if self.iname == "ADD.D" or self.iname == "SUB.D":
             self.functionalUnit = "FP adder"
             self.execCycle = int(functional_units[self.functionalUnit][0])
-                        
-            
-    
-        
-class Pipeline:
-     def __init__(self, instructions):
-         
-         
-         fpRegistersList = []
-         instObjs = []
-         for i in instructions:
-             print(i)
-             instObjs.append(Instructions(i))
-             
-             
-         #Iterating Over the instruction objects
-         processor = Processor()
-         x = 30
-         cycle = 0
-         while(x>0):
-             cycle +=1
-             for inst in instObjs:
-                 if(inst.status != 4):
-                     if(inst.status == 0):
-                         if(processor.fBusy == "No"):
-            
-                             processor.fBusy = "Yes"
-                             fpRegistersList.append(inst.op1)                            
-                             inst.fetch = cycle
-                             inst.status = 1
-                                 
-                                 
-                                 
-                     elif(inst.status == 1):
-                         if(processor.dBusy == "No" ):
-                             if(not(inst.op2 in fpRegistersList) and not(inst.op3 in fpRegistersList)):                                                                 
-                                 processor.dBusy = "Yes"
-                                 processor.fBusy = "No"
-                                 inst.decode = cycle
-                                 inst.status = 2
-                             else:
-                                 inst.raw = "Y"
-                             
-                     elif(inst.status == 2):                        
-                         if(inst.iname == "L.D"):
-                             
-                                                              
-                             if(processor.intBusy == "No" and inst.intCount == 1):
-                                 processor.intBusy = "Yes" 
-                                 inst.intCount -=1
-                                 processor.dBusy = "No"
-                             
-                             elif(processor.memBusy == "No"):
-                                 if(inst.memCount == 2):
-                                     inst.memCount = 1
-                                     processor.intBusy = "No"
-                                     processor.memBusy = "Yes"
-                             elif(processor.memBusy == "Yes"):
-                                 if(inst.memCount == 2):
-                                     inst.struct = "Y"
-                                 if(inst.memCount == 1):                                
-                                     inst.memCount = 0
-                                     inst.exec = cycle
 
-                                     inst.status = 3
-                         
-                        
-                        ## YET TO ADD THE HAZARDS BOTH STRUCT AND RAW and make the pipelined auto instead of manual
-                         elif(inst.iname == "ADD.D" or inst.iname == "SUB.D"):
+        if self.iname == "MUL.D":
+            self.functionalUnit = "FP Multiplier"
+            self.execCycle = int(functional_units[self.functionalUnit][0])
+
+
+class Pipeline:
+    def __init__(self, instructions):
+
+        fpRegistersList = []
+        instObjs = []
+        for i in instructions:
+            print(i)
+            instObjs.append(Instructions(i))
+
+        # Iterating Over the instruction objects
+        processor = Processor()
+        x = 31
+        cycle = 0
+        while x > 0:
+            cycle += 1
+            for inst in instObjs:
+                if inst.status != 4:
+                    if inst.status == 0:
+                        if processor.fBusy == "No":
+                            processor.fBusy = "Yes"
+                            fpRegistersList.append(inst.op1)
+                            inst.fetch = cycle
+                            inst.status = 1
+
+
+
+                    elif inst.status == 1:
+                        if processor.dBusy == "No":
+                            if not (inst.op2 in fpRegistersList) and not (inst.op3 in fpRegistersList):
+                                processor.dBusy = "Yes"
+                                processor.fBusy = "No"
+                                inst.decode = cycle
+                                inst.status = 2
+                            else:
+                                inst.raw = "Y"
+
+                    elif inst.status == 2:
+                        if inst.iname == "L.D":
+
+                            if processor.intBusy == "No" and inst.intCount == 1:
+                                processor.intBusy = "Yes"
+                                inst.intCount -= 1
+                                processor.dBusy = "No"
+
+                            elif processor.memBusy == "No":
+                                if inst.memCount == 2:
+                                    inst.memCount = 1
+                                    processor.intBusy = "No"
+                                    processor.memBusy = "Yes"
+                            elif processor.memBusy == "Yes":
+                                if inst.memCount == 2:
+                                    inst.struct = "Y"
+                                if inst.memCount == 1:
+                                    inst.memCount = 0
+                                    inst.exec = cycle
+
+                                    inst.status = 3
+
+
+                        ## YET TO ADD THE HAZARDS BOTH STRUCT  and make the pipelined auto instead of manual
+                        elif inst.iname == "ADD.D" or inst.iname == "SUB.D":
                             pipelined = functional_units[inst.functionalUnit][1]
-                            
-                            if(pipelined == "yes"):
-                                if(processor.addBusy == "No"):                                    
+                            if pipelined == "yes":
+                                if processor.addBusy == "No":
                                     processor.dBusy = "No"
                                     processor.addBusy = "Yes"
                                 inst.execCycle -= 1
-                                if(inst.execCycle == 0):
+                                if inst.execCycle == 0:
                                     inst.exec = cycle
                                     inst.status = 3
-                            
-                            else:        
-                                if(processor.addBusy == "No"):                                    
+
+                            else:
+                                if processor.addBusy == "No":
                                     processor.dBusy = "No"
                                     processor.addBusy = "Yes"
-                                    print("This is",inst.execCycle)
                                     inst.execCycle -= 1
-                                    if(inst.execCycle == 0):
+                                    if inst.execCycle == 0:
                                         inst.exec = cycle
                                         inst.status = 3
-                                
-                                elif(processor.addBusy == "Yes"):
-                                    if(inst.execCycle != 4):
+
+                                elif processor.addBusy == "Yes":
+                                    if inst.execCycle != 4:
                                         inst.execCycle -= 1
-                                        if(inst.execCycle == 0):
+                                        if inst.execCycle == 0:
                                             inst.exec = cycle
                                             inst.status = 3
-                                            
-                         
-                             
-                                        
 
-                            
-                            
-                            
-                                     
-                     elif(inst.status == 3):
-                         if(inst.iname == "L.D"):
-                             processor.memBusy = "No"                            
-                             inst.status = 4
-                         elif(inst.iname == "ADD.D" or inst.iname == "SUB.D"):
-                             processor.addBusy = "No"
-                             inst.status = 4
-                         inst.write = cycle
-                         fpRegistersList.remove(inst.op1)
-                            
-                                                                                                                            
-                             
-                             
-                     
-                     
-                     
-                 
-             x -= 1
-         
-         for i in instObjs:
-             print(i.fetch,i.decode,i.exec, i.write,i.raw, i.struct)
-         print(functional_units)
-         print(functional_units.values())
-         
+                        elif inst.iname == "MUL.D":
+                            pipelined = functional_units[inst.functionalUnit][1]
+                            if pipelined == "yes":
+                                if processor.mulBusy == "No":
+
+                                    processor.dBusy = "No"
+                                    processor.mulBusy = "Yes"
+                                inst.execCycle -= 1
+                                if inst.execCycle == 0:
+                                    inst.exec = cycle
+                                    inst.status = 3
+
+
+                            else:
+                                if processor.mulBusy == "No":
+                                    processor.dBusy = "No"
+                                    processor.mulBusy = "Yes"
+                                    inst.execCycle -= 1
+                                    if inst.execCycle == 0:
+                                        inst.exec = cycle
+                                        inst.status = 3
+                                elif processor.mulBusy == "Yes":
+                                    if inst.execCycle != 4:
+                                        inst.execCycle -= 1
+                                        if inst.execCycle == 0:
+                                            inst.exec = cycle
+                                            inst.status = 3
+                    elif inst.status == 3:
+                        if inst.iname == "L.D":
+                            processor.memBusy = "No"
+                            inst.status = 4
+                        elif inst.iname == "ADD.D" or inst.iname == "SUB.D":
+                            processor.addBusy = "No"
+                            inst.status = 4
+                        elif inst.iname == "MUL.D":
+                            processor.mulBusy = "No"
+                            inst.status = 4
+                        inst.write = cycle
+                        fpRegistersList.remove(inst.op1)
+            x -= 1
+
+        for i in instObjs:
+            print(i.fetch, i.decode, i.exec, i.write, i.raw, i.struct)
+        print(functional_units)
+        print(functional_units.values())
+
 
 class Processor:
     def __init__(self):
@@ -179,19 +189,13 @@ class Processor:
         self.memBusy = "No"
         self.addBusy = "No"
         self.mulBusy = "No"
-                
-         
-
-
 
 
 if __name__ == "__main__":
     regs = readFiles("D:\\Fall 2019\\ACA\\Project\\reg.txt")
-    dataLoc = readFiles("D:\\Fall 2019\\ACA\\Project\\data.txt")    
+    dataLoc = readFiles("D:\\Fall 2019\\ACA\\Project\\data.txt")
     functional_units = readFiles("D:\\Fall 2019\\ACA\\Project\\config.txt")
     loop, instructions = readFiles("D:\\Fall 2019\\ACA\\Project\\inst.txt")
-    
+
     print(loop)
     Pipeline(instructions)
-    
-      
--- utils.py
+++ utils.py
@@ -7,77 +7,68 @@ This is a temporary script file.
 
 
 def readFiles(path):
-    f = open(path,"r")
+    f = open(path, "r")
     r = f.read()
     r = r.split("\n")
-    
-    if("reg.txt" in path):
-        
+
+    if "reg.txt" in path:
+
         for i in range(len(r)):
-            r[i] = int(r[i],2)
+            r[i] = int(r[i], 2)
         for i in range(len(r)):
             r[i] = hex(r[i])
         return r
-    elif("data.txt" in path):
-    
+    elif "data.txt" in path:
+
         j = 100
         for i in range(len(r)):
-            r[i] = ('0x'+str(j), int(r[i],2))
-            j+=1
+            r[i] = ('0x' + str(j), int(r[i], 2))
+            j += 1
         return r
-
-    
-    elif("config.txt" in path):
+    elif "config.txt" in path:
         functional_units = functionalUnits(r)
         return functional_units
-    
-    elif("inst.txt" in path):
-        loop,instructions = instParsing(r)
-        
-        
-        return loop,instructions
-    
+
+    elif "inst.txt" in path:
+        loop, instructions = instParsing(r)
+        return loop, instructions
+
+
 def functionalUnits(units):
     dict1 = {}
     for i in range(len(units)):
         key = units[i].split(":")[0]
         value1 = units[i].split(":")[1].split(",")[0][1:]
-        if("," in units[i]):
+        if "," in units[i]:
             value2 = units[i].split(":")[1].split(",")[1][1:]
             dict1[key] = (value1, value2)
         else:
-            dict1[key] = (value1)
+            dict1[key] = value1
     return dict1
+
 
 def instParsing(insts):
     loop = []
     instructions = []
-    print("inst",insts)
+    print("inst", insts)
     for i in range(len(insts)):
-        if(":" in insts[i]):
+        if ":" in insts[i]:
             loop.append([insts[i].split(":")[0], i])
             x = insts[i].split(":")[1][1:].split()
-            if(len(x) == 3):
-                instructions.append([x[0],x[1][0:-1],x[2]])
-            elif(len(x) == 4):
+            if len(x) == 3:
+                instructions.append([x[0], x[1][0:-1], x[2]])
+            elif len(x) == 4:
                 instructions.append([x[0], x[1][0:-1], x[2][0:-1], x[3]])
             else:
                 instructions.append([x[0]])
-            
+
         else:
             x = insts[i].split()
-            if(len(x) == 3):
-                instructions.append([x[0],x[1][0:-1],x[2]])
-            elif(len(x) == 4):
+            if len(x) == 3:
+                instructions.append([x[0], x[1][0:-1], x[2]])
+            elif len(x) == 4:
                 instructions.append([x[0], x[1][0:-1], x[2][0:-1], x[3]])
             else:
                 instructions.append([x[0]])
-        
-    return loop,instructions
 
-
-
-        
-    
- 
-        
+    return loop, instructions
